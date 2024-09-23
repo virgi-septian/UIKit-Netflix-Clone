0b81fbee2378c97840c72ec702df506cfe019547
virgi-septian <virgiseptian77@gmail.com>
2024-09-23 11:11:10 +0700

    Creating Upcoming TableView Inside Upcoming Tab

--- Netflix Clone/Controllers/UpcomingViewController.swift
+++ Netflix Clone/Controllers/UpcomingViewController.swift
@@ -8,9 +8,58 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
+    
+    private let upcomingTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Upcoming"
         view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationBar.topItem?.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetcUpcoming()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+    
+    private func fetcUpcoming() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "unknown"
+        return cell
     }
 }
--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -20,33 +20,18 @@ class APICaller {
     //Ini adalah cara untuk mengakses instance singleton dari class APICaller. Anda tidak perlu membuat instance baru setiap kali ingin memanggil fungsi di class tersebut. Cukup gunakan instance tunggal yang sudah ada, yaitu shared.
     static let shared = APICaller()
     
-    // Enum untuk mengelola endpoint
-    enum Endpoint: String {
-        case trendingMovies = "/3/trending/movie/day?"
-        case trendingTVs = "/3/trending/tv/day?"
-        case upcomingMovies = "/3/movie/upcoming?language=en-US&page=1"
-        case popularMovies = "/3/movie/popular?language=en-US&page=1"
-        case topRatedMovies = "/3/movie/top_rated?language=en-US&page=1"
-    }
-
-    // Fungsi umum untuk memanggil API
-    private func fetchData<T: Decodable>(
-        from endpoint: Endpoint,
-        completion: @escaping (Result<[T], APIEror>) -> Void
-    ) {
-        guard let url = URL(string: "\(Constant.baseUrl)\(endpoint.rawValue)&api_key=\(Constant.API_KEY)") else {
-            return
-        }
+    func getTrendingMovies(completion: @escaping (Result<[Title], APIEror>)-> Void) {
+        guard let url = URL(string: "\(Constant.baseUrl)/3/trending/movie/day?api_key=\(Constant.API_KEY)") else { return }
         
         let task = URLSession.shared.dataTask(with: url) { data, _, error in
             guard let data = data, error == nil else {
-                completion(.failure(.failedToGetData))
                 return
             }
-            
+             
             do {
+                //Inisialization and then Decoder
                 let result = try JSONDecoder().decode(TrendingTitleResponse.self, from: data)
-                completion(.success(result.results as! [T]))
+                completion(.success(result.results))
             } catch {
                 completion(.failure(.failedToGetData))
             }
@@ -54,24 +39,76 @@ class APICaller {
         
         task.resume()
     }
-
-    func getTrendingMovies(completion: @escaping (Result<[Title], APIEror>) -> Void) {
-        fetchData(from: .trendingMovies, completion: completion)
-    }
-
-    func getTrendingTVs(completion: @escaping (Result<[Title], APIEror>) -> Void) {
-        fetchData(from: .trendingTVs, completion: completion)
+    
+    func getTrendingTVs(completion: @escaping (Result<[Title], APIEror>)-> Void) {
+        guard let url = URL(string: "\(Constant.baseUrl)/3/trending/tv/day?api_key=\(Constant.API_KEY)") else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(TrendingTitleResponse.self ,from: data)
+                completion(.success(result.results))
+            } catch {
+                completion(.failure(.failedToGetData))
+            }
+        }
+        
+        task.resume()
     }
     
-    func getUpcomingMovies(completion: @escaping (Result<[Title], APIEror>) -> Void) {
-        fetchData(from: .upcomingMovies, completion: completion)
+    func getUpcomingMovies(completion: @escaping (Result<[Title], APIEror>)-> Void) {
+        guard let url = URL(string: "\(Constant.baseUrl)/3/movie/upcoming?api_key=\(Constant.API_KEY)&language=en-US&page=1") else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(TrendingTitleResponse.self ,from: data)
+                completion(.success(result.results))
+            } catch {
+                completion(.failure(.failedToGetData))
+            }
+        }
+        
+        task.resume()
     }
     
-    func getPopular(completion: @escaping (Result<[Title], APIEror>) -> Void) {
-        fetchData(from: .popularMovies, completion: completion)
+    func getPopular(completion: @escaping (Result<[Title], APIEror>)-> Void) {
+        guard let url = URL(string: "\(Constant.baseUrl)/3/movie/popular?api_key=\(Constant.API_KEY)&language=en-US&page=1") else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(TrendingTitleResponse.self ,from: data)
+                completion(.success(result.results))
+            } catch {
+                completion(.failure(.failedToGetData))
+            }
+        }
+        
+        task.resume()
     }
-
-    func getTopRated(completion: @escaping (Result<[Title], APIEror>) -> Void) {
-        fetchData(from: .topRatedMovies, completion: completion)
+    
+    func getTopRated(completion: @escaping (Result<[Title], APIEror>)-> Void) {
+        guard let url = URL(string: "\(Constant.baseUrl)/3/movie/top_rated?api_key=\(Constant.API_KEY)&language=en-US&page=1") else { return }
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(TrendingTitleResponse.self ,from: data)
+                completion(.success(result.results))
+            } catch {
+                completion(.failure(.failedToGetData))
+            }
+        }
+        
+        task.resume()
     }
 }
